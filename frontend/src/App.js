76c6c6a041ecc67f6765b00fdc704112bb10cb35
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2023-08-02 17:25:37 -0500

    Initializing frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+    <div className="">
+      <Button variant="primary">Primary</Button>{" "}
+    </div>
+  );
+}
+
+export default App;
