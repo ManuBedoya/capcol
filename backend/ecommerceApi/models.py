11e255d96f63f6ce44fe96ce22df35dac0ae94d4
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2023-09-03 15:22:57 -0500

    Change image field

--- backend/ecommerceApi/models.py
+++ backend/ecommerceApi/models.py
@@ -15,7 +15,8 @@ class Product(models.Model):
     price = models.FloatField()
     applyVariants = models.BooleanField(blank=True, null=True)
     variants = models.CharField(max_length=200, blank=True, null=True)
-    img = models.ImageField(upload_to=upload_to, blank=True, null=True)
+    # img = models.ImageField(upload_to=upload_to, blank=True, null=True)
+    img = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
