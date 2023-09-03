a14a88d6dc5505500a53d7872ae3313e9aa82d53
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2023-09-03 14:58:53 -0500

    Remove log print and add backend images

--- frontend/src/components/general/ProductsHome.jsx
+++ frontend/src/components/general/ProductsHome.jsx
@@ -15,7 +15,6 @@ export default function ProductsHome() {
       .get(urlGetProducts)
       .then(function (response) {
         const { data } = response;
-        console.log(data);
         setProducts(data);
       })
       .catch(function (error) {
